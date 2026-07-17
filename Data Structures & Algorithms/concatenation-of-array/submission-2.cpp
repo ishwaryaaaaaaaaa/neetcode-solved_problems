e55807eb8c090164615c4d4a6780c56eae73d7c6
Ishwarya Mohan <ishwaryamohan.teamkartkgp@gmail.com>
2026-07-17 16:13:30 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int>s;
+        for(int i = 0 ; i < 2*n ; i++){
+            if(i < n){
+                s.push_back(nums[i]);
+
+            }
+            else if (i >= n){
+                s.push_back(nums[i-n]);
+            }
+            
+        
+        
+        }
+        return s ;
+        
+
+        
+    }    
+};
